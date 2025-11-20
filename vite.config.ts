1d4a75c2255cab908632b1ddb6dc704951746fc2
mathursunit <92265686+mathursunit@users.noreply.github.com>
2025-11-20 04:09:39 -0500

    delete old

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// GitHub Pages friendly: relative base
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
